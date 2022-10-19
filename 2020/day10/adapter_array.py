8a58fddd5de0fd370b4d5dacb35eddf041458a30
Ben Riggs <briggs@sancsoft.com>
2022-10-19 12:44:20 -0400

    2020.10

--- 2020/day10/adapter_array.py
+++ 2020/day10/adapter_array.py
@@ -1,7 +1,9 @@
+import itertools
+import functools
 import numpy as np
 
 
-with open('2020/day10/small.txt') as f:
+with open('2020/day10/data.txt') as f:
     adapters = np.array([int(line) for line in f.read().splitlines()])
 
 x = np.append(adapters, (0, max(adapters) + 3))
@@ -18,10 +20,17 @@ def num_choices(ar, jolts) -> int:
     diffs = ar - jolts
     return np.count_nonzero((diffs >= 1) & (diffs <= 3))
 
+@functools.lru_cache()
+def branches(chunk) -> int:
+    if chunk == tuple(): return 1
+    elif chunk == (2,): return 2
+    elif chunk == (3, 2): return 4
+    return branches(chunk[1:]) + branches(chunk[2:]) + branches(chunk[3:])
 
-#adapters[adapters == 1] = 3
-print(adapters)
 choices = np.array([num_choices(adapters, x) for x in adapters])
-print(choices)
 choices[choices == 1] = 0
-print(choices)
+
+chunks = [list(x[1]) for x in itertools.groupby(choices, lambda x: x == 0) if not x[0]]
+branch_list = [branches(tuple(chunk)) for chunk in chunks]
+num_branches = functools.reduce(lambda x, y: x*y, branch_list)
+print(f'PART TWO: {num_branches}')
