a0964b89fcc5ad944130b8e5bcf3e0e0ec0c5c68
philophoseal <benjamin.riggs.38@gmail.com>
2022-10-19 23:58:05 -0400

    day 11

--- 2020/day11/seating_system.py
+++ 2020/day11/seating_system.py
@@ -1,3 +1,4 @@
+from enum import Enum, auto
 import numpy as np
 
 FLOOR = '.'
@@ -5,60 +6,91 @@ EMPTY_SEAT = 'L'
 OCCUPIED_SEAT = '#'
 
 
-def count_adjacent_seats(layout):
-    occ = layout == OCCUPIED_SEAT
-    left, right = np.pad(occ, ((0,0), (1,0)))[:, :-1], np.pad(occ, ((0,0), (0,1)))[:, 1:]
-    down, up = np.pad(occ, ((1,0), (0,0)))[:-1, :], np.pad(occ, ((0,1), (0,0)))[1:, :]
-    down_left, down_right = np.pad(up, ((0,0), (1,0)))[:, :-1], np.pad(up, ((0,0), (0,1)))[:, 1:]
-    up_left, up_right = np.pad(down, ((0,0), (1,0)))[:, :-1], np.pad(down, ((0,0), (0,1)))[:, 1:]
+with open('2020/day11/data.txt') as f:
+    INITIAL_LAYOUT = np.array([list(line) for line in f.read().splitlines()])
+
+
+class Direction(Enum):
+    UP = auto()
+    DOWN = auto()
+    LEFT = auto()
+    RIGHT = auto()
+    UPPER_LEFT = auto()
+    UPPER_RIGHT = auto()
+    LOWER_LEFT = auto()
+    LOWER_RIGHT = auto()
+
+
+def adjacent_seats(layout, direction: Direction):
+    match direction:
+        case Direction.UP: retval = np.pad(layout, ((0,1), (0,0)))[1:, :]
+        case Direction.DOWN: retval = np.pad(layout, ((1,0), (0,0)))[:-1, :]
+        case Direction.LEFT: retval = np.pad(layout, ((0,0), (1,0)))[:, :-1]
+        case Direction.RIGHT: retval = np.pad(layout, ((0,0), (0,1)))[:, 1:]
+        case Direction.UPPER_LEFT: retval = np.pad(layout, ((0,1), (1,0)))[1:, :-1]
+        case Direction.UPPER_RIGHT: retval = np.pad(layout, ((0,1), (0,1)))[1:, 1:]
+        case Direction.LOWER_LEFT: retval = np.pad(layout, ((1,0), (1,0)))[:-1, :-1]
+        case Direction.LOWER_RIGHT: retval = np.pad(layout, ((1,0), (0,1)))[:-1, 1:]
+        case _: raise Exception('Invalid direction')
+    retval[retval == '0'] = FLOOR
+    return retval
     
-    return sum([
-        left, right, down, up, down_left, down_right, up_left, up_right
-    ])
+def count_adjacent_seats(layout, direction: Direction) -> int:
+    occ = layout == OCCUPIED_SEAT
+    return adjacent_seats(occ, direction=direction)
 
 def occupy_seats(layout, count_method=count_adjacent_seats):
-    adjacent = count_method(layout)
+    seats = sum([count_method(layout, d) for d in Direction])
 
     retval = np.copy(layout)
-    retval[(retval == EMPTY_SEAT) & (adjacent == 0)] = OCCUPIED_SEAT
+    retval[(retval == EMPTY_SEAT) & (seats == 0)] = OCCUPIED_SEAT
     return retval
 
 def leave_seats(layout, tolerance, count_method=count_adjacent_seats):
-    adjacent = count_method(layout)
+    seats = sum([count_method(layout, d) for d in Direction])
 
     retval = np.copy(layout)
-    retval[(retval == OCCUPIED_SEAT) & (adjacent >= tolerance)] = EMPTY_SEAT
+    retval[(retval == OCCUPIED_SEAT) & (seats >= tolerance)] = EMPTY_SEAT
     return retval
-
-
-with open('2020/day11/small.txt') as f:
-    layout = np.array([list(line) for line in f.read().splitlines()])
     
 
+layout1 = np.copy(INITIAL_LAYOUT)
 while True:
-    step_one = occupy_seats(layout)
+    step_one = occupy_seats(layout1)
     step_two = leave_seats(step_one, tolerance=4)
 
-    if np.array_equal(layout, step_two):
+    if np.array_equal(layout1, step_two):
         break
-    layout = step_two
+    layout1 = step_two
 
-occupied_count = np.count_nonzero(layout == OCCUPIED_SEAT)
-print(layout)
+occupied_count = np.count_nonzero(layout1 == OCCUPIED_SEAT)
 print(f'PART ONE: {occupied_count}')
 
 
-def count_seen_seats(layout):
-    pass
+def sightline(layout, direction: Direction):
+    retval = np.copy(layout)
+    shifted = adjacent_seats(layout, direction)
+    _slice = np.where(retval == FLOOR)
+    retval[_slice] = shifted[_slice]
 
-# while True:
-#     step_one = occupy_seats(layout, count_method=count_seen_seats)
-#     step_two = leave_seats(step_one, tolerance=5, count_method=count_seen_seats)
+    if not np.array_equal(layout, retval):
+        return sightline(retval, direction)
+    return retval
 
-#     if np.array_equal(layout, step_two):
-#         break
-#     layout = step_two
+def count_seen_seats(layout, direction: Direction) -> int:
+    seen_seats = sightline(layout, direction)
+    occ = seen_seats == OCCUPIED_SEAT
+    return adjacent_seats(occ, direction)
 
-# occupied_count = np.count_nonzero(layout == OCCUPIED_SEAT)
-# print(layout)
-# print(f'PART TWO: {occupied_count}')
+
+layout2 = np.copy(INITIAL_LAYOUT)
+while True:
+    step_one = occupy_seats(layout2, count_method=count_seen_seats)
+    step_two = leave_seats(step_one, tolerance=5, count_method=count_seen_seats)
+
+    if np.array_equal(layout2, step_two):
+        break
+    layout2 = step_two
+
+occupied_count = np.count_nonzero(layout2 == OCCUPIED_SEAT)
+print(f'PART TWO: {occupied_count}')
