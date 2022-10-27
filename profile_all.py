ae0f3ed12fdd68dabb027ebc5edc5ad9a76ade15
Ben Riggs <briggs@sancsoft.com>
2022-10-27 12:30:55 -0400

    Adding profiling

--- profile_all.py
+++ profile_all.py
@@ -7,4 +7,5 @@ for f in py_files:
     if 'profile_all' in str(f) or '2021' in str(f):
         continue
     output = f.parent.joinpath(f'{f.stem}.pstats')
+    print(f'**** {str(f)}')
     os.system(f'python -m cProfile -o {output.as_posix()} {f.as_posix()}')
