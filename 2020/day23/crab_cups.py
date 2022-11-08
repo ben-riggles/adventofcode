29c8ff528b934033900136ab86cbcbd4b5069ae5
Ben Riggs <benjamin.riggs.38@gmail.com>
2022-11-08 11:08:12 -0500

    more days

--- 2020/day23/crab_cups.py
+++ 2020/day23/crab_cups.py
@@ -1,29 +1,66 @@
 import numpy as np
 from numpy.typing import NDArray
+from typing import Dict, List
 
 
-with open('2020/day23/small.txt') as f:
-    cups = np.array([int(x) for x in f.read()])
-    
-def move(cups: NDArray, max_val: int) -> NDArray:
-    current_cup = cups[0]
-    moving_cups = cups[1:4]
-    cups = np.delete(cups, slice(1,4))
-    dest_idx = np.argmax((cups - current_cup) % max_val) + 1
-    cups = np.concatenate([cups[:dest_idx], moving_cups, cups[dest_idx:]])
-    return np.roll(cups, -1)
+with open('2020/day23/data.txt') as f:
+    cups = [int(x) for x in f.read()]
 
 
-cups1 = cups.copy()
+def destination(current: int, moving: List[int], max_cup: int) -> int:
+    while True:
+        current = current -1 if current > 1 else max_cup
+        if current not in moving:
+            return current
+
+
+def move(cups: Dict[int,int], current_cup: int, max_cup: int) -> int:
+    next1 = cups[current_cup]
+    next2 = cups[next1]
+    next3 = cups[next2]
+    next4 = cups[next3]
+    moving_cups = [next1, next2, next3]
+    dest = destination(current_cup, moving_cups, max_cup)
+
+    cups[current_cup] = next4
+    cups[next3] = cups[dest]
+    cups[dest] = next1
+    return next4
+
+
+cup_dict = {}
+try:
+    for idx, cup in enumerate(cups):
+        cup_dict[cup] = cups[idx+1]
+except IndexError:
+    cup_dict[cups[-1]] = cups[0]
+
+current_cup = cups[0]
 for turn in range(100):
-    cups1 = move(cups1, 9)
-cups1 = np.roll(cups1, -1 * np.where(cups1 == 1)[0])
-print(f'PART ONE: {"".join(str(x) for x in cups1[1:])}')
+    current_cup = move(cup_dict, current_cup, 9)
+
+cup_list = [cup_dict[1]]
+while cup_list[-1] != 1:
+    cup_list.append(cup_dict[cup_list[-1]])
+cup_list = ''.join([str(x) for x in cup_list[:-1]])
+print(f'PART ONE: {cup_list}')
 
 
-cups2 = np.concatenate((cups, np.arange(start=10, stop=1000001)))
+cup_dict = {}
+try:
+    for idx, cup in enumerate(cups):
+        cup_dict[cup] = cups[idx+1]
+except IndexError:
+    cup_dict[cups[-1]] = 10
+
+for i in range(10, 1000000):
+    cup_dict[i] = i+1
+cup_dict[1000000] = cups[0]
+
+current_cup = cups[0]
 for turn in range(10000000):
-    cups2 = move(cups2, 1000000)
-idx1 = np.where(cups2 == 1)[0]
-next1, next2 = cups2[idx1+1], cups2[idx1+2]
-print(f'PART TWO: {next1 * next2}')
+    current_cup = move(cup_dict, current_cup, 1000000)
+
+next1 = cup_dict[1]
+next2 = cup_dict[next1]
+print(f'PART TWO: {next1*next2}')
