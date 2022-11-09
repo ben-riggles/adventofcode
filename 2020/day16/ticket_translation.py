8ae4024a6bafbb8930347bbade497f50c30cb8fb
philophoseal <benjamin.riggs.38@gmail.com>
2022-11-09 18:20:04 -0500

    More utils changes

--- 2020/day16/ticket_translation.py
+++ 2020/day16/ticket_translation.py
@@ -1,87 +1,74 @@
 from __future__ import annotations
+import aoc
+from dataclasses import dataclass, field
 from functools import reduce
 import numpy as np
 import re
-from typing import Set, List
 
 
+@dataclass
 class Field:
-    def __init__(self, name: str, values: Set[int]):
-        self.name: str = name
-        self.values: Set[int] = values
-        self.idx: int = -1
+    name: str
+    values: set[int] = field(default_factory=set, repr=False)
+    idx: int = field(default=-1, init=False, repr=False)
 
-    def __eq__(self, other: Field) -> bool:
-        return self.name == other.name
-
-    def match(self, other: Set[int]) -> bool:
+    def match(self, other: set[int]) -> bool:
         return other <= self.values
 
     @staticmethod
-    def parse(field_str: str) -> Field:
-        m = re.match(r'(?P<name>.*): (?P<min_a>.*)-(?P<max_a>.*) or (?P<min_b>.*)-(?P<max_b>.*)', field_str)
-        result = m.groupdict()
-
-        set_a = set(range(int(result['min_a']), int(result['max_a']) + 1))
-        set_b = set(range(int(result['min_b']), int(result['max_b']) + 1))
-        return Field(name=result['name'], values=set_a | set_b)
-
+    def from_string(field_str: str) -> Field:
+        data = re.match(r'(?P<name>.*): (?P<min_a>.*)-(?P<max_a>.*) or (?P<min_b>.*)-(?P<max_b>.*)', field_str).groupdict()
+        set_a = set(range(int(data['min_a']), int(data['max_a']) + 1))
+        set_b = set(range(int(data['min_b']), int(data['max_b']) + 1))
+        return Field(name=data['name'], values=set_a | set_b)
 
 class Ticket:
-    def __init__(self, values: List[int]):
+    def __init__(self, values: list[int]):
         self.values = values
 
-    def error_rate(self, valid_values: Set[int]) -> int:
+    def error_rate(self, valid_values: set[int]) -> int:
         return sum(x for x in self.values if x not in valid_values)
 
     @staticmethod
-    def parse(ticket_str: str) -> Ticket:
+    def from_string(ticket_str: str) -> Ticket:
         values = [int(x) for x in ticket_str.split(',')]
         return Ticket(values)
+        
 
-
-with open('2020/day16/data.txt') as f:
-    fields, my_ticket, tickets = f.read().split('\n\n')
-
-fields = fields.splitlines()
-my_ticket = my_ticket.splitlines()[1]
-tickets = tickets.splitlines()[1:]
-
-fields = [Field.parse(f) for f in fields]
-valid_values = set.union(*[x.values for x in fields])
-
-tickets = [Ticket.parse(t) for t in tickets]
-error = sum(t.error_rate(valid_values) for t in tickets)
-print(f'PART ONE: {error}')
-
-
-valid_tickets = [t for t in tickets if t.error_rate(valid_values) == 0]
-value_grid = np.array([t.values for t in valid_tickets]).T
-ticket_fields = {idx: set(x) for idx, x in enumerate(value_grid)}
-
-
-def next_field(fields: List[Field], current: Field) -> Field | None:
-    curr_idx = fields.index(current)
-    next_idx = curr_idx + 1
-    while next_idx != curr_idx:
-        next_idx %= len(fields)
-        if fields[next_idx].idx < 0:
-            return fields[next_idx]
-        next_idx += 1
-    return None
-
-field = fields[0]
-while field is not None:
-    matches = [idx for idx, x in ticket_fields.items() if field.match(x)]
-    if len(matches) == 0:
-        raise Exception('We failed')
-    elif len(matches) == 1:
+def match_fields(ticket_fields: dict[int, set[int]], fields: list[Field]) -> list[Field]:
+    unmatched = fields.copy()
+    while unmatched:
+        field = unmatched.pop(0)
+        matches = [idx for idx, x in ticket_fields.items() if field.match(x)]
+        if len(matches) > 1:
+            unmatched.append(field)
+            continue
+        
         field.idx = matches[0]
-        ticket_fields.pop(field.idx)
-    field = next_field(fields, field)
+        ticket_fields.pop(matches[0])
+    return fields
 
-my_ticket = Ticket.parse(my_ticket)
-departure_fields = [f for f in fields if f.name.startswith('departure')]
-departure_values = [my_ticket.values[f.idx] for f in departure_fields]
-my_departure = reduce(lambda x,y: x*y, departure_values)
-print(f'PART TWO: {my_departure}')
+
+def main():
+    fields, my_ticket, tickets = aoc.read_chunks()
+
+    fields = [Field.from_string(f) for f in fields.splitlines()]
+    my_ticket = Ticket.from_string(my_ticket.splitlines()[1])
+    tickets = [Ticket.from_string(t) for t in tickets.splitlines()[1:]]
+
+    valid_values = set.union(*[x.values for x in fields])
+    part1 = sum(t.error_rate(valid_values) for t in tickets)
+
+    valid_tickets = [t for t in tickets if t.error_rate(valid_values) == 0]
+    value_grid = np.array([t.values for t in valid_tickets]).T
+    ticket_field_values = {idx: set(x) for idx, x in enumerate(value_grid)}
+
+    fields = match_fields(ticket_field_values, fields)
+    departure_fields = [f for f in fields if f.name.startswith('departure')]
+    departure_values = [my_ticket.values[f.idx] for f in departure_fields]
+    part2 = reduce(lambda x,y: x*y, departure_values)
+
+    aoc.print_results(part1, part2)
+
+if __name__ == '__main__':
+    main()
--- 2020/day17/conway_cubes.py
+++ 2020/day17/conway_cubes.py
@@ -1,3 +1,4 @@
+import aoc
 import itertools
 import numpy as np
 from numpy.typing import NDArray
@@ -6,19 +7,18 @@ from numpy.typing import NDArray
 ACTIVE, INACTIVE = '#', '.'
 NUM_CYCLES = 6
 
-def create_grid(dimensions: int, init: NDArray) -> NDArray:
+def create_grid(dimensions: int, init: NDArray, border: int = NUM_CYCLES) -> NDArray:
     init_hgt, init_wdt = init.shape
-    border_size = NUM_CYCLES + 1
-    grid_size = max(init_hgt, init_wdt) + 2*border_size
+    grid_size = max(init_hgt, init_wdt) + 2 * border
     grid = np.full((grid_size,)*dimensions, INACTIVE)
 
-    slice_x = slice(border_size, border_size + init_wdt)
-    slice_y = slice(border_size, border_size + init_hgt)
+    slice_x = slice(border, border + init_wdt)
+    slice_y = slice(border, border + init_hgt)
     start_slice = (grid_size//2,)*(dimensions-2) + (slice_y, slice_x)
-    grid[start_slice] = initial_state
+    grid[start_slice] = init
     return grid
 
-def active_neighbors(grid: NDArray) -> int:
+def active_neighbors(grid: NDArray) -> NDArray:
     return sum([
         np.roll(grid, tuple(x), axis=tuple(range(grid.ndim))) == ACTIVE 
         for x in itertools.product((-1, 0, 1), repeat=grid.ndim)
@@ -36,20 +36,20 @@ def run_cycle(grid: NDArray) -> NDArray:
     return retval
 
 
-with open('2020/day17/data.txt') as f:
-    initial_state = np.array([list(line) for line in f.read().splitlines()])
+def main():
+    initial_state = np.array(list(map(list, aoc.read_lines())))
 
-grid1 = create_grid(dimensions=3, init=initial_state)
-for _ in range(NUM_CYCLES):
-    grid1 = run_cycle(grid1)
+    grid1 = create_grid(dimensions=3, init=initial_state)
+    for _ in range(NUM_CYCLES):
+        grid1 = run_cycle(grid1)
+    part1 = np.count_nonzero(grid1 == ACTIVE)
 
-active_cubes = np.count_nonzero(grid1 == ACTIVE)
-print(f'PART ONE: {active_cubes}')
+    grid2 = create_grid(dimensions=4, init=initial_state)
+    for _ in range(NUM_CYCLES):
+        grid2 = run_cycle(grid2)
+    part2 = np.count_nonzero(grid2 == ACTIVE)
 
+    aoc.print_results(part1, part2)
 
-grid2 = create_grid(dimensions=4, init=initial_state)
-for _ in range(NUM_CYCLES):
-    grid2 = run_cycle(grid2)
-
-active_cubes = np.count_nonzero(grid2 == ACTIVE)
-print(f'PART TWO: {active_cubes}')
+if __name__ == '__main__':
+    main()
