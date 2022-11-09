26ac525507205cf6e885c1d5799d8b154e35d03e
Ben Riggs <briggs@sancsoft.com>
2022-11-09 16:28:38 -0500

    more changes

--- 2020/day15/rambunctious_recitation.py
+++ 2020/day15/rambunctious_recitation.py
@@ -1,25 +1,30 @@
-from lib2to3.pgen2.token import NUMBER
+import aoc
 
 
-AGES = {}
+def play_game(starters: list[int], total_turns: int) -> int:
+    asdf = starters.copy()
 
+    ages = {}
+    turn = 0
+    value = 0
+    next_turn = 0
+    while turn < total_turns:
+        try:
+            value = asdf.pop(0)
+        except IndexError:
+            value = next_turn
+            
+        next_turn = turn - ages.get(value, turn)
+        ages[value] = turn
+        turn += 1
+    return value
 
-with open('2020/day15/data.txt') as f:
-    starters = [int(x) for x in f.read().split(',')]
+def main():
+    starters = list(map(int, aoc.read_data().split(',')))
+    part1 = play_game(starters, 2020)
+    part2 = play_game(starters, 30_000_000)
+    aoc.print_results(part1, part2)
 
-NUMBER_OF_TURNS = 30000000
-
-turn = 0
-value = 0
-next_turn = 0
-while turn < NUMBER_OF_TURNS:
-    try:
-        value = starters.pop(0)
-    except IndexError:
-        value = next_turn
-        
-    next_turn = turn - AGES.get(value, turn)
-    AGES[value] = turn
-    turn += 1
-print(f'PART ONE: {value}')
+if __name__ == '__main__':
+    main()
     
