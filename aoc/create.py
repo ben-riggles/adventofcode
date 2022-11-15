2bec3219b0d48b562b82332cadc782d0cbb2bee3
Ben Riggs <briggs@sancsoft.com>
2022-11-15 12:35:33 -0500

    Adding create script

---
+++ aoc/create.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path
+
+
+def create(year: int, day: int, name: str):
+    year_dir = Path.cwd().joinpath(str(year))
+    if not year_dir.exists():
+        year_dir.mkdir()
+    day_dir = year_dir.joinpath('day{:02d}'.format(day))
+    if not day_dir.exists():
+        day_dir.mkdir()
+    python_file = day_dir.joinpath(f'{name}.py')
+    small_file = day_dir.joinpath('small.txt')
+    data_file = day_dir.joinpath('data.txt')
+
+    with open(small_file, mode='w') as f: pass
+    with open(data_file, mode='w') as f: pass
+    with open(python_file, mode='w') as f:
+        f.write(
+            'import aoc\n\n\n' + 
+            'def main():\n' + 
+            '\taoc.setup(__file__)\n\n' +
+            'if __name__ == \'__main__\':\n' +
+            '\tmain()\n'
+        )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a new Advent of Code folder and associated files')
+    parser.add_argument('-y', '--year', help='The year to place the new files under', required=True, type=int)
+    parser.add_argument('-d', '--day', help='The day to place the new files under', required=True, type=int)
+    parser.add_argument('-n', '--name', help='The name of the new file', required=True)
+    args = vars(parser.parse_args())
+    create(**args)
