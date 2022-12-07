9ab14e9eb54d6cdab12aafd40cda4fa4d1c360eb
Ben Riggs <briggs@sancsoft.com>
2022-12-07 13:21:23 -0500

    class-based day 7

--- 2022/day07/no_space_left_on_device.py
+++ 2022/day07/no_space_left_on_device.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 import aoc
 from collections import Counter
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import Generator
 
 
 @dataclass
@@ -9,87 +10,76 @@ class File:
     name: str
     size: int
 
+@dataclass
 class Directory:
-    TOP_LEVEL: Directory = None
+    name: str = field(default='')
+    parent: Directory = field(default=None, repr=False)
+    contents: dict[str, File|Directory] = field(init=False, repr=False, default_factory=dict)
 
-    def __init__(self, name: str, parent: Directory = None):
-        self.name = name
-        self.parent = parent
-        self.content = {}
+    def __getitem__(self, key: str) -> File | Directory:
+        if key == '..': return self.parent
+        if key == '/':  return self.top
 
-    def __repr__(self):
-        return f'Directory({self.name})'
-
-    def __getitem__(self, key: str):
-        if key == '..':
-            return self.parent
-        elif key == '/':
-            return Directory.TOP_LEVEL
-        elif key not in self.content:
-            self.content = Directory(name=key, parent=self)
-        return self.content[key]
-
-    def __setitem__(self, key: str, value):
-        self.content[key] = value
-
-    def add_content(self, content: list[str]):
-        for line in content:
-            a, b = line.split()
-            if a == 'dir' and b not in self.content:
-                self.content[b] = Directory(name=b, parent=self)
-            else:
-                self.content[b] = File(name=b, size=int(a))
-        return self
+        if key not in self.contents:
+            self.contents = Directory(name=key, parent=self)
+        return self.contents[key]
 
     @property
-    def full_name(self) -> str:
-        if self == Directory.TOP_LEVEL:
-            return '/'
-
-        ancestor = self.parent
-        retval = self.name
-        while ancestor is not Directory.TOP_LEVEL:
-            retval = f'{ancestor.name}/{retval}'
-            ancestor = ancestor.parent
-        return f'/{retval}'
+    def path(self) -> str:
+        return '/' + '/'.join(reversed([x.name for x in self.parents()][1:]))
 
     @property
     def size(self) -> int:
-        return sum(x.size for x in self.content.values())
+        return sum(x.size for x in self.contents.values())
 
-Directory.TOP_LEVEL = Directory(name='/')
+    @property
+    def top(self) -> Directory:
+        return list(self.parents())[-1]
+
+    def parents(self) -> Generator[Directory]:
+        level = self
+        while level.parent is not None:
+            yield level
+            level = level.parent
+        yield level
+
+    def add_content(self, content: list[str]) -> Directory:
+        for line in content:
+            a, b = line.split()
+            if a == 'dir' and b not in self.contents:
+                self.contents[b] = Directory(name=b, parent=self)
+            else:
+                self.contents[b] = File(name=b, size=int(a))
+        return self
+
+    def size_report(self) -> Counter[str]:
+        retval = Counter({self.path: self.size})
+        for child in self.contents.values():
+            if isinstance(child, Directory):
+                retval |= child.size_report()
+        return retval
 
 
-
-def build_tree(commands: list[str]) -> dict:
-    cwd = Directory.TOP_LEVEL
-
+def build_tree(commands: list[str]) -> Directory:
+    cwd = Directory()
     for cmd in commands:
         if cmd.startswith('cd'):
             cwd = cwd[cmd.split()[-1]]
         elif cmd.startswith('ls'):
             cwd = cwd.add_content(cmd.splitlines()[1:])
-    return Directory.TOP_LEVEL
-
-def directory_sizes(folder: Directory) -> Counter[str]:
-    retval = Counter({folder.full_name: folder.size})
-    for child in folder.content.values():
-        if isinstance(child, File):
-            continue
-        retval.update(directory_sizes(child))
-    return retval
+    return cwd.top
 
 
 @aoc.register(__file__)
 def answers():
-    terminal = aoc.read_data().split('$ ')
-    directory_tree = build_tree(terminal)
-    sizes = directory_sizes(directory_tree)
-    yield sum([x for x in sizes.values() if x <= 100_000])
+    commands = aoc.read_data().split('$ ')
+    top_level = build_tree(commands)
 
-    unused_space = 70_000_000 - sizes['/']
-    needed_space = 30_000_000 - unused_space
-    yield min([x for x in sizes.values() if x >= needed_space])
+    sizes = top_level.size_report()
+    yield sum(x for x in sizes.values() if x <= 100_000)
+
+    needed_space = 30_000_000 - (70_000_000 - sizes['/'])
+    yield min(x for x in sizes.values() if x >= needed_space)
 
 if __name__ == '__main__':
     aoc.run()
