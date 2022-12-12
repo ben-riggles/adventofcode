4a4b0770050905ed2443c597e5ad77224bcad8fe
Ben Riggs <briggs@sancsoft.com>
2022-12-12 12:37:48 -0500

    Final cleanup

--- 2022/day12/hill_climbing_algorithm.py
+++ 2022/day12/hill_climbing_algorithm.py
@@ -1,11 +1,13 @@
 import aoc
-from dataclasses import dataclass, field
-import heapq
+from collections import deque
 import numpy as np
 from numpy.typing import NDArray
 
 
 Point = tuple[int, int]
+HIGHEST = ord('z') - ord('a') + 2
+ELEVATION_A = 1
+LOWEST = 0
 
 def adjacent_points(shape: tuple[int,int], point: Point) -> set[Point]:
     adj = [
@@ -14,71 +16,47 @@ def adjacent_points(shape: tuple[int,int], point: Point) -> set[Point]:
     ]
     return {p for p in adj if (0 <= p[0] < shape[0]) and (0 <= p[1] < shape[1])}
 
-def manhattan_dist(point1: Point, point2: Point) -> int:
-    return abs(point2[0] - point1[0]) + abs(point2[1] - point1[1])
-
-
-@dataclass(order=True, kw_only=True)
-class Node:
-    estimated: int = 0
-    true_value: int = 0
-    point: Point = field(compare=False)
-    visited: set[Point] = field(compare=False, default_factory=set)
-
-def astar(grid: NDArray, start: Point, end_value: int) -> int:
-    start_node = Node(point=start)
+def bfs(grid: NDArray, start: Point, end_value: int):
     start_value = grid[start]
-    end_points = list(zip(*np.where(grid == end_value)))
     increasing = start_value < end_value
+    
+    queue = deque([(0, start)])
+    visited = set()
 
-    path_stack = []
-    heapq.heappush(path_stack, start_node)
+    while queue:
+        distance, point = queue.popleft()
 
-    while path_stack:
-        node: Node = heapq.heappop(path_stack)
-        if node.point in end_points:
-            return node.true_value
-        if node.point in node.visited:
+        point_val = grid[point]
+        if grid[point] == end_value:
+            return distance
+        if point in visited:
             continue
-        point_val = grid[node.point]
-        node.visited.add(node.point)
+        visited.add(point)
 
-        for adj in adjacent_points(grid.shape, node.point) - node.visited:
+        for adj in adjacent_points(grid.shape, point) - visited:
             adj_val = grid[adj]
             diff = (adj_val - point_val) * (1 if increasing else -1)
             if diff > 1:
                 continue
-            
-            for end in end_points:
-                man_dist = manhattan_dist(end, adj)
-                modifier = -5 if diff == 1 else diff
-
-                new_node = Node(
-                    estimated = man_dist + modifier + node.true_value,
-                    true_value = node.true_value + 1,
-                    point = adj,
-                    visited = node.visited
-                )
-                heapq.heappush(path_stack, new_node)
-
+            queue.append((distance + 1, adj))
 
 def convert_letter(char: str) -> int:
     match char:
-        case 'S': return 0
-        case 'E': return 27
-        case _: return ord(char) - ord('a') + 1
+        case 'S': return LOWEST
+        case 'E': return HIGHEST
+        case _: return ord(char) - ord('a') + ELEVATION_A
 
 
 @aoc.register(__file__)
 def answers():
     height_map = np.vectorize(convert_letter)(aoc.read_grid())
 
-    start = list(zip(*np.where(height_map == 0)))[0]
-    yield astar(height_map, start=start, end_value=27)
+    start = list(zip(*np.where(height_map == LOWEST)))[0]
+    yield bfs(height_map, start, HIGHEST)
 
-    height_map[height_map == 0] = 1
-    start = list(zip(*np.where(height_map == 27)))[0]
-    yield astar(height_map, start=start, end_value=1)
+    height_map[height_map == LOWEST] = ELEVATION_A
+    start = list(zip(*np.where(height_map == HIGHEST)))[0]
+    yield bfs(height_map, start, ELEVATION_A)
 
 if __name__ == '__main__':
     aoc.run()
