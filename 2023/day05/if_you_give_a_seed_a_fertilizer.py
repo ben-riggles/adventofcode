a33c534bbb1204a61ccf2be3f7d73d76e194c297
Ben Riggs <briggs@sancsoft.com>
2023-12-05 11:58:15 -0500

    day 5 readme and edge cases

--- 2023/day05/if_you_give_a_seed_a_fertilizer.py
+++ 2023/day05/if_you_give_a_seed_a_fertilizer.py
@@ -20,29 +20,35 @@ class MapRule:
     def __contains__(self, val: int | Range) -> bool:
         match val:
             case int(): return self.src_start <= val < self.src_end
-            case Range(): return self.src_start <= val.start < self.src_end
+            case Range(): return val.end > self.src_start and val.start < self.src_end
             
     def apply(self, val: int | Range) -> int:
         match val:
             case int(): return (val - self.src_start) + self.dest_start
-            case Range(): return Range(self.apply(val.start), self.apply(val.end))
+            case Range(): return Range(
+                start = self.apply(max(val.start, self.src_start)),
+                end = self.apply(min(val.end, self.src_end)),
+            )
 
 class AlmanacMap:
     def __init__(self, rules: list[MapRule]):
         self.rules = rules
-    
-    def convert(self, values: Iterable[int | Range]) -> Generator[int | Range]:
-        def __convert(val: int | Range) -> Generator[int | Range]:
-            for rule in self.rules:
-                if val in rule:
-                    yield rule.apply(val)
-                    if type(val) is Range and val.end not in rule:
-                        yield from __convert(Range(rule.src_end, val.end))
-                    return
-            yield val
 
+    def convert(self, val: int | Range) -> Generator[int | Range]:
+        for rule in self.rules:
+            if val in rule:
+                yield rule.apply(val)
+                if type(val) is Range:
+                    if val.end-1 not in rule:
+                        yield from self.convert(Range(rule.src_end, val.end))
+                    if val.start not in rule:
+                        yield from self.convert(Range(val.start, rule.src_start))
+                return
+        yield val
+    
+    def convert_all(self, values: Iterable[int | Range]) -> Generator[int | Range]:
         for v in values:
-            yield from __convert(v)
+            yield from self.convert(v)
 
     @staticmethod
     def from_string(map_str: str) -> AlmanacMap:
@@ -63,11 +69,11 @@ def answers():
     seeds = list(map(int, data[0].split(':')[1].split()))
     maps = [AlmanacMap.from_string(x) for x in data[1:]]
     
-    part_one = reduce(lambda x, y: y.convert(x), maps, seeds)
+    part_one = reduce(lambda x, y: y.convert_all(x), maps, seeds)
     yield min(part_one)
 
     part_two = [Range(start=start, end=start+_len) for start, _len in pairwise(seeds)]
-    part_two = reduce(lambda x, y: y.convert(x), maps, part_two)
+    part_two = reduce(lambda x, y: y.convert_all(x), maps, part_two)
     yield min([x.start for x in part_two])
 
 if __name__ == '__main__':
