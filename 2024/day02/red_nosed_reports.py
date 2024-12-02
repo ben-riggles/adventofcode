5f03e68c34da86727634cb5f7504fcdaded76b29
Ben Riggs <briggs@sancsoft.com>
2024-12-02 10:27:48 -0500

    Cleaning up 2024 day 2

--- 2024/day02/red_nosed_reports.py
+++ 2024/day02/red_nosed_reports.py
@@ -1,39 +1,22 @@
 import aoc
+from itertools import pairwise
 
 
-def is_safe(report: list[int], ignore_one: bool = False):
-    def _check_level(idx: int, previous: int = None, direction: bool = None, ignore_used: bool = False) -> bool:
-        try:
-            value = report[idx]
-        except IndexError:
-            return True
-        
-        if ignore_one and not ignore_used and _check_level(idx+1, previous, direction, True):
-            return True
-        
-        if previous is None:
-            return _check_level(idx+1, value, None, ignore_used)
-        
-        diff = previous - value
-        dir = diff > 0
-        if (direction is None or direction == dir) and 1 <= abs(diff) <= 3:
-            return _check_level(idx+1, value, dir, ignore_used)
-        
-        return False
-        
-    return _check_level(0)
+def check_report(report: list[int], tolerate: bool = False) -> bool:
+    def _safe(r) -> bool:
+        diffs = [y - x for (x, y) in pairwise(r)]
+        min_diff, max_diff = min(diffs), max(diffs)
+        return (max_diff <= 3 and min_diff >= 1) or (max_diff <= -1 and min_diff >= -3)
+    
+    if tolerate:
+        return any(_safe(report[:i] + report[i+1:]) for i in range(len(report)))
+    return _safe(report)
 
 @aoc.register(__file__)
 def answers():
     reports = [list(map(int, x.split())) for x in aoc.read_lines()]
-
-    # Part One
-    safe_reports = [is_safe(x) for x in reports]
-    yield sum(safe_reports)
-
-    # Part Two
-    safe_reports = [is_safe(x, ignore_one=True) for x in reports]
-    yield sum(safe_reports)
+    yield sum(check_report(x) for x in reports)
+    yield sum(check_report(x, tolerate=True) for x in reports)
 
 if __name__ == '__main__':
     aoc.run()
