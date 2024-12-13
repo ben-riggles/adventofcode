75e3dee78a61e6674a2965adf0eca807ae08f86a
Ben Riggs <briggs@sancsoft.com>
2024-12-13 09:05:37 -0500

    2024 day 12

--- 2024/day12/garden_groups.py
+++ 2024/day12/garden_groups.py
@@ -1,9 +1,9 @@
 from __future__ import annotations
 import aoc
 from aoc.grid import Grid, Point, Direction
-from collections import deque
+from collections import deque, defaultdict
 from dataclasses import dataclass, field
-from functools import reduce
+import itertools
 from typing import Iterator
 
 
@@ -13,6 +13,7 @@ class GardenPlot(Grid[str]):
         plant: str
         points: set[Point] = field(default_factory=set)
         perimeter: int = 0
+        sides: int = 0
 
         def __repr__(self):
             return f'Region(plant={self.plant}, area={self.area}, perimeter={self.perimeter})'
@@ -21,31 +22,26 @@ class GardenPlot(Grid[str]):
         def area(self) -> int:
             return len(self.points)
         
-        @property
-        def sides(self) -> int:
-            edges = tuple((d.movement * 0.5) for d in Direction)
-            borders = [{(p + e) for e in edges} for p in self.points]
-            borders: set[tuple[Point, Direction]] = reduce(lambda x, y: x ^ y, borders, set())
-            border_count = 0
-            while borders:
-                b = borders.pop()
-                border_count += 1
-                travel = ((Direction.UP, Direction.DOWN) if isinstance(b.x, int) else (Direction.LEFT, Direction.RIGHT))
-                for t in travel:
-                    n = b + t.movement
-                    while n in borders:
-                        borders.remove(n)
-                        n += t.movement
-            return border_count
-        
         def fence_price(self, discount: bool = False) -> int:
             return self.area * (self.perimeter if not discount else self.sides)
-
+        
+    
+    @staticmethod
+    def __count_sides(direction: Direction, points: list[Point]) -> int:
+        diffs = ((direction.rotate(1).movement, direction.rotate(-1).movement))
+        sorter = (lambda p: (p.x, p.y)) if direction in (Direction.LEFT, Direction.RIGHT) else (lambda p: (p.y, p.x))
+        borders = sorted(points, key=sorter)
+        num_sides = 1
+        for p1, p2 in itertools.pairwise(borders):
+            if not any((p2 == p1 + diffs[0], p2 == p1 + diffs[1])):
+                num_sides += 1
+        return num_sides
         
     def __map_region(self, point: Point, value: str) -> tuple[GardenPlot.Region, set[Point]]:
         visited = set()
         queue = deque([point])
         region = GardenPlot.Region(plant=value)
+        sides = defaultdict(list)
 
         while queue:
             p = queue.pop()
@@ -54,16 +50,19 @@ class GardenPlot(Grid[str]):
             visited.add(p)
             region.points.add(p)
 
-            for adj in {p + d.movement for d in Direction}:
+            for d in Direction:
+                adj = p + d.movement
                 if not self.binds(adj):
-                    region.perimeter += 1
+                    sides[d].append(p)
                 elif self[adj] != value:
-                    region.perimeter += 1
+                    sides[d].append(p)
                     visited.add(adj)
                 elif adj not in visited:
                     queue.append(adj)
-        return region, visited - region.points
 
+        region.perimeter = sum(len(x) for x in sides.values())
+        region.sides = sum(self.__count_sides(d, x) for d, x in sides.items())
+        return region, visited - region.points
         
     def regions(self) -> Iterator[GardenPlot.Region]:
         point = Point(0, 0)
@@ -81,11 +80,8 @@ class GardenPlot(Grid[str]):
 
 @aoc.register(__file__)
 def answers():
-    garden = GardenPlot(aoc.read_grid('e'))
+    garden = GardenPlot(aoc.read_grid())
     regions = tuple(garden.regions())
-
-    print(regions)
-    print([(r, r.sides) for r in regions])
 
     yield sum(r.fence_price() for r in regions)
     yield sum(r.fence_price(discount=True) for r in regions)
