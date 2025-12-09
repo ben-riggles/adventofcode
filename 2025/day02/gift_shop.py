49ae21d499090238af55c22ff84e88dd41459b89
philophoseal <benjamin.riggs.38@gmail.com>
2025-12-09 00:13:13 -0500

    Final day 2 changes

--- 2025/day02/gift_shop.py
+++ 2025/day02/gift_shop.py
@@ -5,66 +5,47 @@ from itertools import count
 from typing import Generator
 
 
-def invalid_ids(max_value: int, only_doubles: bool = True) -> Generator[int]:
-    for value in count(1):
-        for repeat in count(2):
-            extended = int(str(value) * repeat)
-            if extended > max_value:
-                if repeat == 2:
-                    return
-                break
-            yield extended
+def invalid_ids(max_value: int) -> Generator[(int, int)]:
+    max_digits = len(str(max_value))
+    for i in count(1):
+        s = str(i) * 2
+        v = int(s)
+        if v > max_value:
+            return
+        yield v, 1
+        while len(s) <= max_digits:
+            s += str(i)
+            yield int(s), 2
 
-            if only_doubles:
-                break
-
-def in_ranges(ranges: list[Interval], value: int) -> bool:
-    return any(value in x for x in ranges)
+def invalid_in_range(r: Interval, invalid_ids: list[int]) -> list[int]:
+    try:
+        start = next(i for i, x in enumerate(invalid_ids) if x >= r.start)
+        end = next(i for i, x in enumerate(invalid_ids) if x > r.end)
+    except StopIteration:
+        return [0]
+    
+    in_range = invalid_ids[start:end]
+    invalid_ids = invalid_ids[end:]
+    return in_range
 
 
 @aoc.register(__file__)
 def answers():
     ranges = [Interval.parse(x) for x in aoc.read_data().replace('\n', '').split(',')]
     max_value = max(x.end for x in ranges)
-    max_digits = len(str(max_value))
 
-    invalid_1 = set()
-    invalid_2 = set()
-    for i in count(1):
-        s = str(i) * 2
-        v = int(s)
-        if v > max_value:
-            break
-        invalid_1.add(v)
-        while len(s) <= max_digits:
-            invalid_2.add(int(s))
-            s += str(i)
-    invalid_1 = sorted(invalid_1)
-    invalid_2 = sorted(invalid_2)
-    ranges = sorted(ranges, key=lambda x: x.start)
+    all_invalid = sorted(invalid_ids(max_value))
+    invalid_1 = [x for x, part in all_invalid if part == 1]
+    invalid_2 = [x for x, _ in all_invalid]
 
-    a = 0
-    b = 0
+    result_1 = 0
+    result_2 = 0
     for r in ranges:
-        try:
-            invalid_start_1 = next(i for i, x in enumerate(invalid_1) if x >= r.start)
-            invalid_end_1 = next(i for i, x in enumerate(invalid_1) if x > r.end)
-            _invalid_1 = invalid_1[invalid_start_1:invalid_end_1]
-            invalid_1 = invalid_1[invalid_end_1:]
-            a += sum(_invalid_1)
-        except StopIteration:
-            pass
+        result_1 += sum(invalid_in_range(r, invalid_1))
+        result_2 += sum(invalid_in_range(r, invalid_2))
 
-        try:
-            invalid_start_2 = next(i for i, x in enumerate(invalid_2) if x >= r.start)
-            invalid_end_2 = next(i for i, x in enumerate(invalid_2) if x > r.end)
-            _invalid_2 = invalid_2[invalid_start_2:invalid_end_2]
-            invalid_2 = invalid_2[invalid_end_2:]
-            b += sum(_invalid_2)
-        except StopIteration:
-            pass
-    yield a
-    yield b
+    yield result_1
+    yield result_2
 
 if __name__ == '__main__':
     aoc.run()
